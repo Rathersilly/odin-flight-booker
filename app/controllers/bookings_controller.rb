a105a7314365e08ab8e475b0624323eace00f6f7
rathersilly <rathersilly@live.com>
2021-03-26 02:13:33 -0600

    finished assignment but 0 polish

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,19 +5,37 @@ class BookingsController < ApplicationController
   end
 
   def new
+    # new is for new instance of specific model
+    # build is for creating a new instance withing an AR association (apparently)
     @booking = Booking.new
   end
   def create
-    @booking = Booking.create!(booking_params)
+    puts "CREATING"
+    p params
+    p booking_params
+    p booking_params[:passenger]
+
+    @passenger = Passenger.create(passenger_params)
+    @booking = Booking.new(flight_id: booking_params[:flight_id],
+                           passenger_id: @passenger.id)
+
     if @booking.save
+      puts "SAVED"
+      redirect_to root_url
       # redirect etc
     else
+      puts "NOT SAVED"
+      flash[:warning] = "somethings wrong"
+      redirect_to root_url
     end
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:flight, :passenger)
+    params.require(:booking).permit(:flight_id)
+  end
+  def passenger_params
+    params.require(:passenger).permit(:name, :email)
   end
 
 end
